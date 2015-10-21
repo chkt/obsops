4d2722f7c235fd19dd0a2ca8f65c19f687581f51
chkt <ck@christoph-kettelhoit.de>
2015-10-21 15:04:15 +0200

    Updated tests for .intersectionByKey, .differenceByKey Added tests for .intersectionByValue, .differenceByValue

--- test/difference.js
+++ test/difference.js
@@ -4,23 +4,23 @@ import * as _obj from '../source';
 
 
 
-describe('obj.difference', () => {
+describe('obj.differenceByKey', () => {
 	it("should require at least one argument", () => {
 		_assert.throws(() => {
-			_obj.difference();
+			_obj.differenceByKey();
 		}, TypeError);
 	});
 
 	it("should require all arguments to be objects", () => {
 		_assert.throws(() => {
-			_obj.difference({}, null);
+			_obj.differenceByKey({}, null);
 		}, TypeError);
 	});
 
 	it("should create a empty object from a single argument", () => {
 		const op1 = { a : 1, b : 2 };
 
-		_assert.deepStrictEqual(_obj.difference(op1), {});
+		_assert.deepStrictEqual(_obj.differenceByKey(op1), {});
 	});
 
 	it("should create a shallow object difference", () => {
@@ -29,7 +29,7 @@ describe('obj.difference', () => {
 		const op3 = { d : 3 };
 		const res = { b : 1 };
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should merge scalars right-to-left", () => {
@@ -38,7 +38,7 @@ describe('obj.difference', () => {
 		const op3 = { a : 3 };
 		const res = { a : 1 };
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should create a deep object difference", () => {
@@ -47,7 +47,7 @@ describe('obj.difference', () => {
 		const op3 = { a : { a : { d : 3 }}};
 		const res = { a : { a : { b : 1 }}};
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should remove empty difference sets", () => {
@@ -55,7 +55,7 @@ describe('obj.difference', () => {
 		const op2 = { a : { a : 2, b : 2 }};
 		const res = {};
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2), res);
 	});
 
 	it("should treat [ a \\ b \\ c ] as [ a \\ (b \\ c) ]", () => {
@@ -63,7 +63,7 @@ describe('obj.difference', () => {
 		const op2 = { a : { a : 2, b : 2 }};
 		const op3 = { a : { b : 3, c : 3 }};
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), _obj.difference(op1, _obj.difference(op2, op3)));
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), _obj.differenceByKey(op1, _obj.differenceByKey(op2, op3)));
 	});
 
 	it("should apply type-changes in processing order [ a \\ (b \\ c) ]", () => {
@@ -72,7 +72,7 @@ describe('obj.difference', () => {
 		const op3 = { a : { b : 3 }};
 		const res = { a : { a : 1 }};
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should assign functions by reference", () => {
@@ -84,7 +84,7 @@ describe('obj.difference', () => {
 		const op3 = { a : fnb };
 		const res = { a : fna };
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should create the difference of arrays by index", () => {
@@ -96,7 +96,7 @@ describe('obj.difference', () => {
 		res[0] = 1;
 		res[2] = 1;
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should create the difference of sparse arrays", () => {
@@ -110,7 +110,7 @@ describe('obj.difference', () => {
 		op3[2] = 3, op3[3] = 4;
 		res[0] = 1, res[3] = 4;
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should create the difference of nested arrays by index", () => {
@@ -122,7 +122,7 @@ describe('obj.difference', () => {
 		res.a[0] = 1;
 		res.a[2] = 1;
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2, op3), res);
 	});
 
 	it("should remove empty difference set arrays", () => {
@@ -130,6 +130,135 @@ describe('obj.difference', () => {
 		const op2 = { a : [1] };
 		const res = {};
 
-		_assert.deepStrictEqual(_obj.difference(op1, op2), res);
+		_assert.deepStrictEqual(_obj.differenceByKey(op1, op2), res);
+	});
+});
+
+
+describe('obj.differenceByValue', () => {
+	it("should require at least one argument", () => {
+		_assert.throws(() => {
+			_obj.differenceByValue();
+		}, TypeError);
+	});
+
+	it("should require all arguments to be objects", () => {
+		_assert.throws(() => {
+			_obj.differenceByValue({}, null);
+		}, TypeError);
+	});
+
+	it("should create a empty object from a single argument", () => {
+		const op1 = { a : 1, b : 2 };
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1), {});
+	});
+
+	it("should create a shallow object difference", () => {
+		const op1 = { a : 1, b : 1 };
+		const op2 = { a : 1, c : 2 };
+		const op3 = { d : 3 };
+		const res = { b : 1 };
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should not merge differing scalars", () => {
+		const op1 = { a : 1 };
+		const op2 = { a : 2 };
+		const res = { a : 1 };
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2), res);
+	});
+
+	it("should create a deep object difference", () => {
+		const op1 = { a : { a : { a : 1, b : 1 }}};
+		const op2 = { a : { a : { a : 1, c : 2 }}};
+		const op3 = { a : { a : { d : 3 }}};
+		const res = { a : { a : { b : 1 }}};
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should remove empty difference sets", () => {
+		const op1 = { a : { a : 1 }};
+		const op2 = { a : { a : 1, b : 2 }};
+		const res = {};
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2), res);
+	});
+
+	it("should treat [ a \\ b \\ c ] as [ a \\ (b \\ c) ]", () => {
+		const op1 = { a : { a : 1, b : 1 }};
+		const op2 = { a : { a : 2, b : 2 }};
+		const op3 = { a : { b : 3, c : 3 }};
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), _obj.differenceByValue(op1, _obj.differenceByValue(op2, op3)));
+	});
+
+	it("should apply type-changes in processing order [ a \\ (b \\ c) ]", () => {
+		const op1 = { a : { a : 1 }};
+		const op2 = { a : null };
+		const op3 = { a : { b : 3 }};
+		const res = { a : { a : 1 }};
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should assign functions by reference", () => {
+		function fna() {}
+		function fnb() {}
+
+		const op1 = { a : fna };
+		const op2 = { a : fnb };
+		const res = { a : fna };
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2), res);
+	});
+
+	it("should create the difference of arrays by index", () => {
+		const op1 = [1, 1, 1];
+		const op2 = [1, 1];
+		const op3 = [1];
+		const res = [];
+
+		res[0] = 1;
+		res[2] = 1;
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should create the difference of sparse arrays", () => {
+		const op1 = [];
+		const op2 = [];
+		const op3 = [];
+		const res = [];
+
+		op1[0] = 1, op1[3] = 4;
+		op2[1] = 2, op2[3] = 4;
+		op3[2] = 3, op3[3] = 4;
+		res[0] = 1, res[3] = 4;
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should create the difference of nested arrays by index", () => {
+		const op1 = { a : [1, 1, 1] };
+		const op2 = { a : [1, 1] };
+		const op3 = { a : [1] };
+		const res = { a : [] };
+
+		res.a[0] = 1;
+		res.a[2] = 1;
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2, op3), res);
+	});
+
+	it("should remove empty difference set arrays", () => {
+		const op1 = { a : [1] };
+		const op2 = { a : [1] };
+		const res = {};
+
+		_assert.deepStrictEqual(_obj.differenceByValue(op1, op2), res);
 	});
 });
--- test/intersection.js
+++ test/intersection.js
@@ -4,20 +4,20 @@ import * as _obj from '../source';
 
 
 
-describe('obj.intersection', () => {
+describe('obj.intersectionByKey', () => {
 	it("should require at least one argument", () => {
-		_assert.throws(() => { _obj.intersection() }, TypeError);
+		_assert.throws(() => { _obj.intersectionByKey() }, TypeError);
 	});
 
 	it("should require all arguments to be objects", () => {
-		_assert.throws(() => { _obj.intersection({}, null) }, TypeError);
+		_assert.throws(() => { _obj.intersectionByKey({}, null) }, TypeError);
 	});
 
 	it("should create a copy of a single argument", () => {
 		const op1 = { a : 1, b : 2 };
 
-		_assert.deepStrictEqual(_obj.intersection(op1), op1);
-		_assert.notEqual(_obj.intersection(op1), op1);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1), op1);
+		_assert.notEqual(_obj.intersectionByKey(op1), op1);
 	});
 
 	it("should create a shallow object intersection", () => {
@@ -26,7 +26,7 @@ describe('obj.intersection', () => {
 		const op3 = { a : 1, d : 3 };
 		const res = { a : 1 };
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should merge scalars right-to-left", () => {
@@ -35,7 +35,7 @@ describe('obj.intersection', () => {
 		const op3 = { a : 3 };
 		const res = { a : 1 };
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should create a deep object intersection", () => {
@@ -44,7 +44,7 @@ describe('obj.intersection', () => {
 		const op3 = { a : { a : { a : 1, d : 3 }}};
 		const res = { a : { a : { a : 1 }}};
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should treat non-intersecting objects as empty", () => {
@@ -52,7 +52,7 @@ describe('obj.intersection', () => {
 		const op2 = { a : null };
 		const res = { a : {}};
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2), res);
 	});
 
 	it("should treat [ a ∩ b ∩ c ] as [ a ∩ (b ∩ c) ]", () => {
@@ -60,7 +60,7 @@ describe('obj.intersection', () => {
 		const op2 = { a : { b : 2 }};
 		const op3 = { a : { b : 3, c : 3 }};
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), _obj.intersection(op1, _obj.intersection(op2, op3)));
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), _obj.intersectionByKey(op1, _obj.intersectionByKey(op2, op3)));
 	});
 
 	it("should apply type-changes in processing order [ a ∩ (b ∩ c) ]", () => {
@@ -69,7 +69,7 @@ describe('obj.intersection', () => {
 		const op3 = { a : { b : 3 }};
 		const res = { a : {}};
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should assign functions by reference", () => {
@@ -80,7 +80,7 @@ describe('obj.intersection', () => {
 		const op2 = { a : fnb };
 		const res = { a : fna };
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2), res);
 	});
 
 	it("should intersect arrays by index", () => {
@@ -89,7 +89,7 @@ describe('obj.intersection', () => {
 		const op3 = [3, 3, 3];
 		const res = [1];
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should intersect sparse arrays", () => {
@@ -103,7 +103,7 @@ describe('obj.intersection', () => {
 		op3[2] = 3, op3[3] = 4;
 		res[3] = 4;
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should intersect nested arrays by index", () => {
@@ -112,7 +112,7 @@ describe('obj.intersection', () => {
 		const op3 = { a : [3, 3, 3] };
 		const res = { a : [1] };
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2, op3), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2, op3), res);
 	});
 
 	it("should treat nonintersecting arrays as empty", () => {
@@ -120,6 +120,127 @@ describe('obj.intersection', () => {
 		const op2 = { a : null };
 		const res = { a : [] };
 
-		_assert.deepStrictEqual(_obj.intersection(op1, op2), res);
+		_assert.deepStrictEqual(_obj.intersectionByKey(op1, op2), res);
+	});
+});
+
+
+
+describe('obj.intersectionByValue', () => {
+	it("should require at least one argument", () => {
+		_assert.throws(() => { _obj.intersectionByValue() }, TypeError);
+	});
+
+	it("should require all arguments to be objects", () => {
+		_assert.throws(() => { _obj.intersectionByValue({}, null) }, TypeError);
+	});
+
+	it("should create a copy of a single argument", () => {
+		const op1 = { a : 1, b : 2 };
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1), op1);
+		_assert.notEqual(_obj.intersectionByValue(op1), op1);
+	});
+
+	it("should create a shallow object intersection", () => {
+		const op1 = { a : 1, b : 1 };
+		const op2 = { a : 1, c : 2 };
+		const op3 = { a : 1, d : 3 };
+		const res = { a : 1 };
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should not merge differing scalars", () => {
+		const op1 = { a : 1 };
+		const op2 = { a : 2 };
+		const op3 = { a : 3 };
+		const res = {};
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should create a deep object intersection", () => {
+		const op1 = { a : { a : { a : 1, b : 1 }}};
+		const op2 = { a : { a : { a : 1, c : 2 }}};
+		const op3 = { a : { a : { a : 1, d : 3 }}};
+		const res = { a : { a : { a : 1 }}};
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should treat non-intersecting objects as empty", () => {
+		const op1 = { a : { a : 1 }};
+		const op2 = { a : { b : 1 }};
+		const res = { a : {}};
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2), res);
+	});
+
+	it("should treat [ a ∩ b ∩ c ] as [ a ∩ (b ∩ c) ]", () => {
+		const op1 = { a : { a : 1, b : 1 }};
+		const op2 = { a : { b : 1 }};
+		const op3 = { a : { b : 1, c : 1 }};
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), _obj.intersectionByValue(op1, _obj.intersectionByKey(op2, op3)));
+	});
+
+	it("should apply type-changes in processing order [ a ∩ (b ∩ c) ]", () => {
+		const op1 = { a : { a : 1 }};
+		const op2 = { a : null };
+		const op3 = { a : { b : 3 }};
+		const res = {};
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should assign functions by reference", () => {
+		function fna() {}
+
+		const op1 = { a : fna };
+		const op2 = { a : fna };
+		const res = { a : fna };
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2), res);
+	});
+
+	it("should intersect arrays by index", () => {
+		const op1 = [1];
+		const op2 = [1, 2];
+		const op3 = [1, 2, 3];
+		const res = [1];
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should intersect sparse arrays", () => {
+		const op1 = [];
+		const op2 = [];
+		const op3 = [];
+		const res = [];
+
+		op1[0] = 1, op1[3] = 4;
+		op2[1] = 2, op2[3] = 4;
+		op3[2] = 3, op3[3] = 4;
+		res[3] = 4;
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should intersect nested arrays by index", () => {
+		const op1 = { a : [1] };
+		const op2 = { a : [1, 2] };
+		const op3 = { a : [1, 2, 3] };
+		const res = { a : [1] };
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2, op3), res);
+	});
+
+	it("should treat nonintersecting arrays as empty", () => {
+		const op1 = { a : [1] };
+		const op2 = { a : [2] };
+		const res = { a : [] };
+
+		_assert.deepStrictEqual(_obj.intersectionByValue(op1, op2), res);
 	});
 });
