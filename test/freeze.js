e7b5efc8a477b4ef27179c00e95afe6317dcd79a
chkt <ck@christoph-kettelhoit.de>
2016-02-17 00:01:29 +0000

    Added freezeProxy tests

--- test/freeze.js
+++ test/freeze.js
@@ -151,3 +151,149 @@ describe('obj.freezeCopy', () => {
 		_assert.strictEqual(descb.value, undefined);
 	});
 });
+
+
+
+describe('obj.freezeProxy', () => {
+	it("should require one argument", () => {
+		_assert.doesNotThrow(() =>_obj.freezeProxy({}));
+	});
+
+	it("should require its argument to be an object instance" , () => {
+		_assert.throws(() => _obj.freezeProxy(true), TypeError);
+		_assert.throws(() => _obj.freezeProxy(1), TypeError);
+		_assert.throws(() => _obj.freezeProxy(Symbol()), TypeError);
+		_assert.throws(() => _obj.freezeProxy(null), TypeError);
+		_assert.doesNotThrow(() => _obj.freezeProxy({}));
+		_assert.throws(() => _obj.freezeProxy(() => 1), TypeError);
+	});
+
+	it ("should return an object", () => {
+		const obj = _obj.freezeProxy({});
+
+		_assert(obj instanceof Object && obj.constructor === Object);
+	});
+
+	it("should reference the scalar properties of the source", () => {
+		const source = {
+			a : 1,
+			b : 2,
+			c : 3
+		};
+
+		const res = _obj.freezeProxy(source);
+
+		_assert.strictEqual(source.a, res.a);
+		_assert.strictEqual(source.b, res.b);
+		_assert.strictEqual(source.c, res.c);
+	});
+
+	it("should reference the nested properties of the source", () => {
+		const source = { a : {
+			a : 1,
+			b : 2,
+			c : 3
+		}};
+
+		const res = _obj.freezeProxy(source);
+
+		_assert.strictEqual(source.a.a, res.a.a);
+		_assert.strictEqual(source.a.b, res.a.b);
+		_assert.strictEqual(source.a.c, res.a.c);
+	});
+
+	it("should reflect scalar changes to the source", () => {
+		const source = {
+			a : 1,
+			b : 2,
+			c : 3
+		};
+
+		const res = _obj.freezeProxy(source);
+
+		source.a = 4;
+
+		_assert.strictEqual(res.a, source.a);
+	});
+
+	it("should reflect nested changes to the source", () => {
+		const source = { a : {
+			a : 1,
+			b : 2,
+			c : 3
+		}};
+
+		const res = _obj.freezeProxy(source);
+
+		source.a.a = 4;
+
+		_assert.strictEqual(source.a.a, res.a.a);
+	});
+
+	it("should freeze all scalar properties", () => {
+		const source = {
+			a : 1,
+			b : 2,
+			c : 3
+		};
+
+		const res = _obj.freezeProxy(source);
+
+		_assert.throws(() => res['a'] = 2);
+		_assert.throws(() => res['a'] = {});
+		_assert.throws(() => res['d'] = 4);
+	});
+
+	it("should freeze all nested properties", () => {
+		const source = { a : {
+			a : 1,
+			b : 2,
+			c : 3
+		}};
+
+		const res = _obj.freezeProxy(source);
+
+		_assert.throws(() => res['a']['a'] = 2);
+		_assert.throws(() => res['a']['a'] = {});
+		_assert.throws(() => res['a']['d'] = 4);
+	});
+
+	it("should not change scalar getters to values", () => {
+		const source = {
+			get a() {
+				return 1;
+			}
+		};
+
+		const res = _obj.freezeProxy(source);
+		const desc = Object.getOwnPropertyDescriptor(res, 'a');
+
+		_assert('get' in desc);
+		_assert(!('value' in desc));
+		_assert.strictEqual(res.a, 1);
+	});
+
+	it("should remove setters", () => {
+		const source = {
+			get a() {
+				return 1;
+			},
+
+			set a(value) {},
+
+			set b(value) {}
+		};
+
+		const res = _obj.freezeProxy(source);
+		const desca = Object.getOwnPropertyDescriptor(res, 'a');
+		const descb = Object.getOwnPropertyDescriptor(res, 'b');
+
+		_assert('get' in desca);
+		_assert('set' in desca && desca['set'] === undefined);
+		_assert(!('value' in desca));
+		_assert.strictEqual(res.a, 1);
+		_assert('set' in descb && descb['set'] === undefined);
+		_assert(!('value' in descb));
+		_assert.strictEqual(res.b, undefined);
+	});
+});
